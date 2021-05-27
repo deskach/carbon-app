98669f0ce4265c7de3e187623ff7752ae1149695
Kachanau <dzianiskachanau@ibagroup.eu>
2021-05-27 17:25:17 +0300

    Remove <header /> from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 import { Button } from 'carbon-components-react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
       <Button>Button</Button>
     </div>
   );
