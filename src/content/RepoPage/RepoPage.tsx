37e284f3be7e4d3c7aa8938215ad739ac20eb431
Kachanau <dzianiskachanau@ibagroup.eu>
2021-05-31 10:30:10 +0300

    Populate data table

--- src/content/RepoPage/RepoPage.tsx
+++ src/content/RepoPage/RepoPage.tsx
@@ -1,6 +1,77 @@
 import React from 'react';
 import './_repo-page.scss';
 import RepoTable from "./RepoTable";
+import { gql, OperationVariables } from 'apollo-boost';
+import { DataTableRow, Link } from 'carbon-components-react';
+import { Query, QueryResult } from 'react-apollo';
+
+const REPO_QUERY = gql`
+  query REPO_QUERY {
+    # Let's use carbon as our organization
+    organization(login: "carbon-design-system") {
+      # We'll grab all the repositories in one go. To load more resources
+      # continuously, see the advanced topics.
+      repositories(first: 75, orderBy: { field: UPDATED_AT, direction: DESC }) {
+        totalCount
+        nodes {
+          url
+          homepageUrl
+          issues(filterBy: { states: OPEN }) {
+            totalCount
+          }
+          stargazers {
+            totalCount
+          }
+          releases(first: 1) {
+            totalCount
+            nodes {
+              name
+            }
+          }
+          name
+          updatedAt
+          createdAt
+          description
+          id
+        }
+      }
+    }
+  }
+`;
+
+const LinkList: React.FC<{ url: string; homepageUrl: string }> = ({url, homepageUrl}) => (
+  <ul style={{display: 'flex'}}>
+    <li>
+      <Link href={url}>GitHub</Link>
+    </li>
+    {homepageUrl && (
+      <li>
+        <span>&nbsp;|&nbsp;</span>
+        <Link href={homepageUrl}>Homepage</Link>
+      </li>
+    )}
+  </ul>
+);
+
+interface GitHubRowDescriptor extends DataTableRow {
+  stargazers: Record<string, any>
+  createdAt: Date
+  updatedAt: Date
+  issues: any
+  links: any
+  url: string
+  homepageUrl: string
+}
+
+const getRowItems = <R extends GitHubRowDescriptor>(rows: R[]) => rows.map((row) => ({
+  ...row,
+  key: row.id,
+  stars: row.stargazers.totalCount,
+  issueCount: row.issues.totalCount,
+  createdAt: new Date(row.createdAt).toLocaleDateString(),
+  updatedAt: new Date(row.updatedAt).toLocaleDateString(),
+  links: <LinkList url={row.url} homepageUrl={row.homepageUrl}/>,
+}));
 
 const headers = [
   {
@@ -29,38 +100,26 @@ const headers = [
   },
 ];
 
-const rows = [
-  {
-    id: '1',
-    name: 'Repo 1',
-    createdAt: 'Date',
-    updatedAt: 'Date',
-    issueCount: '123',
-    stars: '456',
-    links: 'Links',
-  },
-  {
-    id: '2',
-    name: 'Repo 2',
-    createdAt: 'Date',
-    updatedAt: 'Date',
-    issueCount: '123',
-    stars: '456',
-    links: 'Links',
-  },
-  {
-    id: '3',
-    name: 'Repo 3',
-    createdAt: 'Date',
-    updatedAt: 'Date',
-    issueCount: '123',
-    stars: '456',
-    links: 'Links',
-  },
-];
-
 const RepoPage = () => {
-  return <RepoTable rows={rows} headers={headers} />
+  return <Query query={REPO_QUERY}>
+    {({ loading, error, data }: QueryResult<any, OperationVariables>) => {
+      // Wait for the request to complete
+      if (loading) return <>'Loading...'</>;
+
+      // // Something went wrong with the data fetching
+      if (error) return <>`Error! ${error.message}`</>;
+
+      // If we're here, we've got our data!
+      const { repositories } = data.organization;
+      const rows = getRowItems(repositories.nodes);
+
+      return (
+        <>
+          <RepoTable headers={headers} rows={rows} />
+        </>
+      );
+    }}
+  </Query>
 };
 
 export default RepoPage;
